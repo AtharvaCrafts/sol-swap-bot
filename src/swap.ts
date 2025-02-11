8c0f0c8f68f290c4c47c75b39b0faf0ae3a4e470
Atharva <katharva2002@gmail.com>
2025-02-11 18:48:35 +0530

    final

--- src/swap.ts
+++ src/swap.ts
@@ -1,34 +1,31 @@
-import { Connection,Keypair,Transaction, VersionedTransaction, sendAndConfirmTransaction } from '@solana/web3.js'
-import bs58 from 'bs58';
-import { NATIVE_MINT } from '@solana/spl-token'
+import bs58 from "bs58";
+import { Connection, Keypair, PublicKey, Transaction, VersionedTransaction } from '@solana/web3.js'
+import { NATIVE_MINT, getAssociatedTokenAddress } from '@solana/spl-token'
 import axios from 'axios'
 import { API_URLS } from '@raydium-io/raydium-sdk-v2'
+const isV0Tx = true;
+const connection = new Connection(process.env.RPC_URL!);
 
-//rpc url
-const connection = new Connection(process.env.RPC_URL!)
-//pvt key
-const owner = Keypair.fromSecretKey(bs58.decode(process.env.PVT_KEY!))
+const owner = Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY!));
 
-//slippage 
-const slippage = 10;
+const slippage = 5;
 
-export async function swap (tokenAddres: string, amount: string){
-
+export async function swap(tokenAddress: string, amount: number) {
+ 
     const { data } = await axios.get<{
         id: string
         success: boolean
         data: { default: { vh: number; h: number; m: number } }
-      }>(`${API_URLS.BASE_HOST}${API_URLS.PRIORITY_FEE}`)
-    
-    const { data: swapResponse } = await axios.get<SwapCompute>(
+      }>(`${API_URLS.BASE_HOST}${API_URLS.PRIORITY_FEE}`);
+
+    const { data: swapResponse } = await axios.get(
         `${
           API_URLS.SWAP_HOST
-        }/compute/swap-base-in?inputMint=${NATIVE_MINT}&outputMint=${tokenAddres}&amount=${amount}&slippageBps=${
+        }/compute/swap-base-in?inputMint=${NATIVE_MINT}&outputMint=${tokenAddress}&amount=${amount}&slippageBps=${
           slippage * 100}&txVersion=V0`
-      ) // Use the URL xxx/swap-base-in or xxx/swap-base-out to define the swap type. 
-      
+    );
 
-      const { data: swapTransactions } = await axios.post<{
+    const { data: swapTransactions } = await axios.post<{
         id: string
         version: string
         success: boolean
@@ -36,13 +33,50 @@ export async function swap (tokenAddres: string, amount: string){
       }>(`${API_URLS.SWAP_HOST}/transaction/swap-base-in`, {
         computeUnitPriceMicroLamports: String(data.data.default.h),
         swapResponse,
-        txVersion : 'V0' ,
+        txVersion: 'V0',
         wallet: owner.publicKey.toBase58(),
         wrapSol: true,
-        unwrapSol: true, // true means output mint receive sol, false means output mint received wsol
-      }) 
+        unwrapSol: false,
+    })
 
-      console.log(swapTransactions); 
+    const ata = await getAssociatedTokenAddress(new PublicKey(tokenAddress), owner.publicKey);
+
+    console.log({
+        computeUnitPriceMicroLamports: String(data.data.default.h),
+        swapResponse,
+        txVersion: 'V0',
+        wallet: owner.publicKey.toBase58(),
+        wrapSol: true,
+        unwrapSol: false,
+        // outputMint: ata.toBase58()
+    })
+    console.log(swapTransactions)
+    const allTxBuf = swapTransactions.data.map((tx) => Buffer.from(tx.transaction, 'base64'))
+    const allTransactions = allTxBuf.map((txBuf) =>
+      isV0Tx ? VersionedTransaction.deserialize(txBuf) : Transaction.from(txBuf)
+    )
+
+    let idx = 0
+    for (const tx of allTransactions) {
+        idx++
+        const transaction = tx as VersionedTransaction
+        transaction.sign([owner])
+
+        const txId = await connection.sendTransaction(tx as VersionedTransaction, { skipPreflight: true })
+        console.log("after sending txn");
+        const { lastValidBlockHeight, blockhash } = await connection.getLatestBlockhash({
+          commitment: 'finalized',
+        })
+        console.log(`${idx} transaction sending..., txId: ${txId}`)
+        await connection.confirmTransaction(
+          {
+            blockhash,
+            lastValidBlockHeight,
+            signature: txId,
+          },
+          'confirmed'
+        )
+        console.log(`${idx} transaction confirmed`)
+    }
+
 }
-
-swap("TOKEN_ADDRESS","AMOUNT");
