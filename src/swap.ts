82efe57e4716bdd7dbb753bab757eb95384451e4
Atharva <katharva2002@gmail.com>
2025-02-11 19:49:48 +0530

    added lamports

--- src/swap.ts
+++ src/swap.ts
@@ -1,7 +1,7 @@
 //This code is written by Atharva Kulkarni
 
 import bs58 from "bs58";
-import { Connection, Keypair, PublicKey, Transaction, VersionedTransaction } from '@solana/web3.js'
+import { Connection, Keypair, PublicKey, Transaction, VersionedTransaction, LAMPORTS_PER_SOL } from '@solana/web3.js'
 import { NATIVE_MINT, getAssociatedTokenAddress } from '@solana/spl-token'
 import axios from 'axios'
 import { API_URLS } from '@raydium-io/raydium-sdk-v2'
@@ -11,6 +11,8 @@ const connection = new Connection(process.env.RPC_URL!);
 const owner = Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY!));
 
 const slippage = 5;
+
+const solanaAmount = 0.001 * LAMPORTS_PER_SOL;
 
 export async function swap(tokenAddress: string, amount: number) {
  
@@ -82,3 +84,4 @@ export async function swap(tokenAddress: string, amount: number) {
     }
 
 }
+swap("ADDRES", solanaAmount);
